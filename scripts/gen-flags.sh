f8cf437128c74e32890d002d2dc2f25872577963
Matej Stastny <matysta@outlook.com>
2025-08-30 20:03:36 -0700

    Fixed flag generating script and improved safety

--- scripts/gen-flags.sh
+++ scripts/gen-flags.sh
@@ -3,13 +3,20 @@
 set -e
 
 CMD="gengetoptions"
-FILE="kittyvault"
+FILE="src/kittyvault"
 
 if ! command -v "$CMD" >/dev/null 2>&1; then
     echo "Missing dependency: $CMD"
     exit 1
 fi
 
+IFS="/"
+read -ra split_path <<<"$(pwd)"
+if [[ "${split_path[${#split_path[@]} - 1]}" != "kittyvault" ]]; then
+    echo "Script not executed in project root"
+    exit 1
+fi
+
 echo "Generating flags for $FILE"
-"$CMD" embed --overwrite kittyvault
-echo "Finished"
+"$CMD" embed --overwrite "$FILE"
+echo "Finished :3"
